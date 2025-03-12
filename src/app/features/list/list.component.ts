1e113c4152ed3e98d4074c7a1fcc77b4c86462a6
josue <josuex.x.x@hotmail.com>
2025-03-12 19:24:42 -0300

    feat(product): add button and link to create new product

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { ProdutosService } from '../../shared/services/produtos.service';
 import { Produto } from '../../shared/interfaces/produto.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
