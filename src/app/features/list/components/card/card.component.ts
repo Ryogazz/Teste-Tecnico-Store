c8a8281c2f8d39aafbb1c495dfa12b662df7947f
josue <josuex.x.x@hotmail.com>
2025-03-13 19:24:16 -0300

    fix: align icon and text properly in "Produto em Destaque" label

--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -2,12 +2,13 @@ import { Component, computed, EventEmitter, input, Output } from '@angular/core'
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+import {MatIconModule} from '@angular/material/icon';
 import { Produto } from '../../../../shared/interfaces/produto.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule,CommonModule],
+  imports: [MatCardModule, MatButtonModule,CommonModule,MatIconModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss'
 })
