a1cb63b885794367bdc99ce3ecb60895c35bc4c8
josue <josuex.x.x@hotmail.com>
2025-03-12 21:14:00 -0300

    style(form): improve layout and styling for product creation form

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,12 +1,64 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+
+  form = new FormGroup({
+    nome: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    }),
+    descricao: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    }),
+    preco: new FormControl<number>(0, {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.min(0)
+      ]
+    }),
+    categoria: new FormControl<string>('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+        Validators.minLength(3)
+      ]
+    }),
+    destaque: new FormControl<boolean>(false, { 
+      nonNullable: true
+    }),
+    dataCriacao: new FormControl<Date>(new Date(), {
+      nonNullable: true
+    }),
+  })
+
+  onSubmit() {
+    console.log(this.form.value)
+  }
+
 
 }
